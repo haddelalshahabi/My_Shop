84a338a958b40545acb12708c39126d78d0b392f
haddelalshahabi <s362080@oslomet.no>
2023-09-15 10:06:41 +0200

    Initial commit

---
+++ MyShop/DAL/IItemRepository.cs
@@ -0,0 +1,12 @@
+﻿using MyShop.Models;
+
+namespace MyShop.DAL;
+
+public interface IItemRepository
+{
+    Task<IEnumerable<Item>> GetAll();
+    Task<Item?> GetItemById(int id);
+    Task Create(Item item);
+    Task Update(Item item);
+    Task<bool> Delete(int id);
+}
---
+++ MyShop/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyShop.DAL;
+using MyShop.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+{
+    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+});
+
+builder.Services.AddDbContext<ItemDbContext>(options => {
+    options.UseSqlite(
+        builder.Configuration["ConnectionStrings:ItemDbContextConnection"]);
+});
+
+builder.Services.AddScoped<IItemRepository, ItemRepository>(); 
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    DBInit.Seed(app);
+}
+
+app.UseStaticFiles();
+
+app.MapDefaultControllerRoute();
+
+app.Run();
